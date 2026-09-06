57f7db3810df860a7f7e821659a9dbcddd18662b
Ruwais1 <liruwais1@gmail.com>
2026-09-06 16:55:18 +0000

    Task 1: Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that provides a type-annotated function concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two strings and returns their concatenation.
+    """
+    return str1 + str2
