41b924d8657d15eb59da28d73a6a02ff1ab70905
Ruwais1 <liruwais1@gmail.com>
2026-09-06 17:21:51 +0000

    Fix async generator type hint

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ Module that contains an asynchronous coroutine async_generator.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Loops 10 times, waits 1 second each time, and yields a random float
     between 0 and 10.
