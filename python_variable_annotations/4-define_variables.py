3bbc467a37192276a19cace038ccfc1ced8c6427
Ruwais1 <liruwais1@gmail.com>
2026-09-06 17:00:20 +0000

    Task 4: Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module that defines and type-annotates specific variables.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
