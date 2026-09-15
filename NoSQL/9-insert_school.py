ae32a2ee42422f5b319928d0e4a1181e645a1595
Ruwais1 <liruwais1@gmail.com>
2026-09-15 17:39:52 +0000

    task 9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""
+Insert a document in Python
+"""
+
+
 
 def insert_school(mongo_collection, **kwargs):
     """
-    inserts a new document in a collcection based on kwargs
+    inserts a new document in a collection based on kwargs
     """
-    
-mongo_collection.insert_one(kwargs)
-return mongon_collection.insert_one(kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs)>inserted_id
