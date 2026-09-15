2c26670f7971c16049319d79a959d686545327a7
Ruwais1 <liruwais1@gmail.com>
2026-09-15 17:19:24 +0000

    task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+List all documents in python
+"""
+
+def list_all documents in Python
+"""
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection 
+    """
+
+
+
+
