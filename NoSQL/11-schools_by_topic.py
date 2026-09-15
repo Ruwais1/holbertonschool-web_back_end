39a816ce2a29c0e2a46ef26197690738074bcb88
Ruwais1 <liruwais1@gmail.com>
2026-09-15 18:13:41 +0000

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/emv python3
+"""
+Where can i learn python?
+"""
+
+
+
+
+def school_by_topics(mongo_collection, topic):
+    """
+    Return the list of school have a specific topic
+    """
+    return(list)mongo_collection.find.topic
