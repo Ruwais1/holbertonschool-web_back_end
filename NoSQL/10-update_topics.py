556f7c324c1ccb10e8e442d5e2c567d17577c506
Ruwais1 <liruwais1@gmail.com>
2026-09-15 18:03:28 +0000

    task 10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -10,8 +10,8 @@ def update_topics(mongo_collection, name, topics):
     Changes all topics of a school document based on the name
     """
     mongo_collection.update_many(
-            {"name": name }
-            {"$set": { "topics": topics } }
+            { "name": name },
+            { "$set": { "topics": topics } }
             )
 
 
